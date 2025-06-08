654ec3e62c9e8cccefaee857ea7494200d527710
samm002 <samuelsdjodis@gmail.com>
2025-06-08 15:32:32 +0700

    refactor: remove message queue and scheduler module

--- apps/message-queue/src/config/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-import { envValues } from './env-values.config';
-
-const { NODE_ENV } = envValues;
-
-export const appConfig = registerAs('app', () => ({
-  env: NODE_ENV,
-}));
--- apps/message-queue/src/config/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './app.config';
-export * from './database.config';
-export * from './env-values.config';
-export * from './env.config';
